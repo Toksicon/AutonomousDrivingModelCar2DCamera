f5c3a44076d33778bbfccac709d7e88cf7e09e81
Tobias Kruse <tobias.kruse@mailbox.org>
2018-11-15 11:25:15 +0100

    [CarTV] Fixed naming of cards in monitor component

--- src/Raspberry Pi/CarTV/src/js/components/monitor.vue
+++ src/Raspberry Pi/CarTV/src/js/components/monitor.vue
@@ -4,7 +4,7 @@
         <div class="col-12">
             <div class="card flex-fill w-100">
                 <div class="card-header">
-                    <h5 class="card-title mb-0">Memory</h5>
+                    <h5 class="card-title mb-0">Viewport</h5>
                 </div>
                 <div class="card-body px-4">
                     <viewport-component />
@@ -17,7 +17,7 @@
         <div class="col-12">
             <div class="card flex-fill w-100">
                 <div class="card-header">
-                    <h5 class="card-title mb-0">CPU</h5>
+                    <h5 class="card-title mb-0">CAN Logger</h5>
                 </div>
                 <div class="card-body px-4">
                     <can-component />
